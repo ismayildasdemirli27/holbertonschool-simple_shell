b01151b7974573407a939ea20124846f797cac9f
ismayildasdemirli27 <dasdemirliismayil40@gmail.com>
2026-07-08 15:55:57 +0000

    Task 2: Simple shell 0.1

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/* execve üçün ətraf mühit dəyişənləri */
+extern char **environ;
+
+#endif /* SHELL_H */
